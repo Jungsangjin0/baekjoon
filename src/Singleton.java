fbeff26f812724d1c01efc6d48731d70daa34ecf
sangjin <loky13@daum.net>
2021-01-18 18:26:49 +0900

    2021-01-18

---
+++ src/Singleton.java
@@ -0,0 +1,20 @@
+
+public class Singleton {
+	
+	private Singleton() {}
+	
+	 /**
+     * static member class
+     * 내부클래스에서 static변수를 선언해야하는 경우 static 내부 클래스를 선언해야만 한다.
+     * static 멤버, 특히 static 메서드에서 사용될 목적으로 선언
+     */
+	private static class InnerInstanceClazz{
+		
+		//클래스 로딩 시점에서 생성
+		private static final Singleton uniqueInstance = new Singleton();
+	}
+	
+	public static Singleton getSingleton() {
+		return InnerInstanceClazz.uniqueInstance;
+	}
+}
